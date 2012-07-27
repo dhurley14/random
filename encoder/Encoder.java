94487034fed1a6e80100f94317cea58dd5f36aad
Devin Hurley <snowmiser111@mac.com>
2012-07-27 12:10:27 -0400

    added a README

--- encoder/Encoder.java
+++ encoder/Encoder.java
@@ -12,13 +12,16 @@ import java.util.Scanner;
  */
 public class Encoder
 {
+    public static char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
+            'q','r','s','t','u','v','w','x','y','z'};
     public static void main(String[] args) throws FileNotFoundException{
         Scanner console = new Scanner(System.in);
         String inputFileName = "mytext.txt";
         String outputFileName = "sendto";
         System.out.print("Input file: " + inputFileName);
-
+        System.out.println();
         System.out.print("Output file: " + outputFileName);
+        System.out.println();
 
         // Construct the Scanner and PrintWriter objects for reading and writing
 
@@ -29,7 +32,7 @@ public class Encoder
 
         out.print(""); // clear "out"
         charOut.print(""); // clear "charOut"
-
+        int x = 0;
         while(in.hasNextLine()){
             String line = in.nextLine();
 
@@ -37,14 +40,27 @@ public class Encoder
             String result = "";
             String saver = "";
             for ( int index = 0 ; index < line.length() ; ++index ){ 
-                if(line.charAt(index) == (' ')){
-                    result += 'K';
+                //if(line.charAt(index) == (' ')){
+                //    result += 'K';
+                // }
+                // if(line.charAt(index) == 'i'){
+                //   result += '!';
+                // }
+                // if(line.charAt(index) != ' ' && line.charAt(index) != 'i'){
+                //   result += line.substring(index,index+1);
+                //}
+                if(line.charAt(index) == ' '){
+                    result += " ";
+                    saver += "K";
                 }
-                if(line.charAt(index) == 'i'){
-                    result += '!';
-                }
-                if(line.charAt(index) != ' ' && line.charAt(index) != 'i'){
-                    result += line.substring(index,index+1);
+                else{
+                    result += alphabet[x]; //index - alphabet.length
+                    x++;
+                    if(x >= alphabet.length){
+                        x=0; // initialize x to zero
+                    }
+                    
+                    saver += line.substring(index,index+1); // saves the chars at that place.
                 }
 
             }
@@ -55,5 +71,11 @@ public class Encoder
         in.close();
         out.close();
         charOut.close();
+    }
+    
+    //method to sort through and find out where in our alphabet the character at that spot lies
+    public static String replacer(char c){
+        
+        return "";
     }
 }
--- encoder/WritingFiles.java
+++ encoder/WritingFiles.java
@@ -13,6 +13,8 @@ import java.util.Scanner;
  */
 public class WritingFiles
 {
+   public static char[] alphabet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
+       'q','r','s','t','u','v','w','x','y','z'};
     public static void main(String[] args) throws FileNotFoundException{
         Scanner console = new Scanner(System.in);
         String inputFileName = "mytext.txt";
@@ -38,6 +40,7 @@ public class WritingFiles
             String result = "";
             String saver = "";
             for ( int index = 0 ; index < line.length() ; ++index ){ 
+                // going to try a caesar cipher of shift one
                 if(line.charAt(index) == (' ')){
                     result += "K";
                 }
