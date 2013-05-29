66d506f73f18b4583c65504f728d08762b5c4e36
Devin Hurley <snowmiser111@mac.com>
2013-05-29 16:06:22 -0400

    added Chat client to profile

---
+++ Chat/Requester.java
@@ -0,0 +1,89 @@
+//The client
+import java.util.Scanner;
+import java.io.*;
+import java.net.*;
+public class Requester{
+    Socket requestSocket;
+    ObjectOutputStream out;
+    ObjectInputStream in;
+    String message;
+    Requester(){}
+
+
+    void run()
+    {
+        try{
+            //1. creating a socket to connect to the server
+            requestSocket = new Socket("localhost", 2004);
+            System.out.println("Connected to localhost in port 2004");
+            //2. get Input and Output streams
+            out = new ObjectOutputStream(requestSocket.getOutputStream());
+            out.flush();
+            Scanner sc = new Scanner(System.in);
+            in = new ObjectInputStream(requestSocket.getInputStream());
+            //3: Communicating with the server
+
+            do{
+                try{
+                    message = (String)in.readObject();
+                    System.out.println("server>" + message);
+                    String msg;
+                    if( message.equals("bye"))break;  
+                    else msg = sc.nextLine();                    
+                    if(msg.equals("bye")){
+                        sendMessage(msg);
+                        break;
+                    }
+                    else sendMessage(msg);
+                    
+                    //sendMessage("Hi my server");
+                    //sendMessage("news");
+                    //message = sc.next();
+                    //sendMessage(message);
+                    //message = sc.next();
+                    //sendMessage(message);
+                }
+                catch(ClassNotFoundException classNot){
+                    System.err.println("data received in unknown format");
+                }
+            }while(!message.equals("bye"));
+        }
+        catch(UnknownHostException unknownHost){
+            System.err.println("You are trying to connect to an unknown host!");
+        }
+        catch(IOException ioException){
+            ioException.printStackTrace();
+        }
+        finally{
+            //4: Closing connection
+            try{
+                in.close();
+                out.close();
+                requestSocket.close();
+            }
+            catch(IOException ioException){
+                ioException.printStackTrace();
+            }
+        }
+    }
+
+
+    void sendMessage(String msg)
+    {
+        try{
+            out.writeObject(msg);
+            out.flush();
+            System.out.println("client>" + msg);
+        }
+        catch(IOException ioException){
+            ioException.printStackTrace();
+        }
+    }
+
+
+    public static void main(String args[])
+    {
+        Requester client = new Requester();
+        client.run();
+    }
+}
